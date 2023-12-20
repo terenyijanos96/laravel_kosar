e2320b68e4631c0b1773b85685f2a098aa4eba2d
terenyijanos96 <terenyi.janos96@gmail.com>
2023-12-20 19:03:38 +0100

    Basket modelben összetett kulcs létrehozása függvénnyel

--- app/Models/Basket.php
+++ app/Models/Basket.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Basket extends Model
 {
     use HasFactory;
+
+    protected function setKeysForSaveQuery($query)
+    {
+        $query
+            ->where('user_id', '=', $this->getAttribute('user_id'))
+            ->where('item_id', '=', $this->getAttribute('copy_id'));
+        return $query;
+    }
+
 }
